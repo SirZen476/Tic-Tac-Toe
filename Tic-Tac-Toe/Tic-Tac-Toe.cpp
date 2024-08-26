96ab32152a1ae9d85c1fadd55f2169209f02bdeb
SirZen476 <danielzenkin82@gmail.com>
2024-08-26 15:46:02 +0300

    added drawboard function, draws empty board

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -2,10 +2,24 @@
 //
 
 #include <iostream>
+using namespace std;
 
+void drawBoard(char board[3][3]) {
+    // function draws playing board from board array
+    cout << "-----------\n";
+    for (int i = 0; i < 3; i++) {
+        cout << "|";
+        for (int j = 0; j < 3;j++) {
+            cout << board[i][j] << " |";
+        }
+        cout << "\n";
+    }
+    cout << "-----------\n";
+}
 int main()
 {
-    std::cout << "Hello World! but test B!\n";
+    char board[3][3] = { {' ',' ',' ',},{' ',' ',' ',},{' ',' ',' ',} };// board used for game
+    drawBoard(board);
 }
 
 
