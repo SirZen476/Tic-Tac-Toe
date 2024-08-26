01275986cd43c54a0ae735796ba56a0784aaa8da
SirZen476 <danielzenkin82@gmail.com>
2024-08-26 16:28:23 +0300

    changed interface, added 2 player mode

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -28,13 +28,13 @@ bool checkWin(char board[3][3], char player){
         return true;
     return false;
 }
-int main(){
+void SinglePlayer() {
     char board[3][3] = { {' ',' ',' ',},{' ',' ',' ',},{' ',' ',' ',} };// board used for game
     char player1 = 'X';
     int turn;
     int row, col;
     bool win = false;
-    cout << "Welcome to Tic-Tac-Toe!/n";
+    cout << "Single-Player Mode\n";
     for (turn = 1; turn < 10; turn++) {// game loop
         drawBoard(board);
         while (true) {
@@ -53,12 +53,68 @@ int main(){
     }
     drawBoard(board);
     if (win) {
-        cout<< "You won in "<< turn << "turns\n";
+        cout << "You won in " << turn << "turns\n";
     }
     else {
         cout << "You Lost\n";
     }
+}
 
+void MultiPlayer() {
+    char board[3][3] = { {' ',' ',' ',},{' ',' ',' ',},{' ',' ',' ',} };// board used for game
+    char player[2] = {'X','O'};
+    int turn;
+    int active = 0;// to track active player - 0 for player 1 ,1 for player 2
+    int row, col;
+    bool win = false;
+    cout << "Multiplayer-Player Mode\n";
+    cout << "Player 1: " << player[0] << "\n";
+    cout << "Player 2: " << player[1] << "\n";
+    for (turn = 1; turn < 10; turn++) {// game loop
+        drawBoard(board);
+        while (true) {
+            cout << "Player " << active+1 << " , Please enter row ,press ENTER and then enter column and press ENTER (range 0 to 2)\n";
+            cin >> row >> col;
+            if (col < 0 || col>2 || row < 0 || row>2 || board[row][col] != ' ') {
+                cout << "Invalid move, Try Again\n";
+            }
+            else break;
+        }
+        board[row][col] = player[active];
+        win = checkWin(board, player[active]);
+        if (win) {
+            break;
+        }
+        //change turns mechanics
+        if (active == 0) active = 1;
+        else active = 0; 
+    }
+    drawBoard(board);
+    if (win) {
+        cout << "Player " <<active+1 <<" won in " << turn << "turns\n";
+    }
+    else {
+        cout << "It's a tie!\n";
+    }
+}
+
+int main(){
+    int players;
+    while (true) {
+        cout << "Welcome to Tic-Tac-Toe! enter 1 for SinglePlayer or 2 for two players\n";
+        cin >> players;
+        if (players == 1) {
+            SinglePlayer();
+            break;
+        }
+        if (players == 2) {
+            MultiPlayer();
+            break;
+        }
+        else {
+            cout << "invalid Input. try again \n";
+        }
+    }
 }
 
 
