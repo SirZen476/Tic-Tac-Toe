c08411380b7c760e4a19440b36d08e9e5197ffe5
SirZen476 <danielzenkin82@gmail.com>
2024-08-26 16:03:20 +0300

    added turn mechanic in main, Chekwin not implemented

--- Tic-Tac-Toe/Tic-Tac-Toe.cpp
+++ Tic-Tac-Toe/Tic-Tac-Toe.cpp
@@ -16,10 +16,30 @@ void drawBoard(char board[3][3]) {
     }
     cout << "-----------\n";
 }
-int main()
-{
+bool checkWin(){
+    //function that checks if player wins. currently for 1 player only
+    return false;
+}
+int main(){
     char board[3][3] = { {' ',' ',' ',},{' ',' ',' ',},{' ',' ',' ',} };// board used for game
-    drawBoard(board);
+    char player1 = 'X';
+    int turn;
+    int row, col;
+    cout << "Welcome to Tic-Tac-Toe!/n";
+    for (turn = 1; turn < 10; turn++) {
+        drawBoard(board);
+        while (true) {
+            cout << "Player " << player1 << " , Please enter row ,press ENTER and then enter column and press ENTER (range 0 to 2)";
+            cin >> row >> col;
+            if (col < 0 || col>2 || row < 0 || row>2 || board[row][col] != ' ') {
+                cout << "Invalid move, Try Again\n";
+            }
+            else break;
+        }
+        board[row][col] = player1;
+
+    }
+
 }
 
 
